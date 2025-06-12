d3af824afcef5e289dadda3d9441b5eccfa9d619
Robles <s.robles.gamboa@accenture.com>
2025-06-12 17:21:51 -0600

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
@@ -11,20 +12,36 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //Middleware
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies to be sent
-  }
-));
+
+if (process.env.NODE_ENV !== 'production') {
+  // Enable CORS for dev environment since we have 2 ports running (frontend and backend)
+  app.use(cors(
+    {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true, // Allow cookies to be sent
+    }
+  ));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 // Connect to  and then start the server
 connectDB().then(() => {
